1370c651d98850bfd67acba31f7bda157bacec8d
Shilpa Shingnapure <shingnapureshilpa2@gmail.com>
2022-01-20 16:43:13 +0530

    product page commit

---
+++ components/footer.js
@@ -0,0 +1,94 @@
+function footer(){
+	return `<div id="footerMain">
+     	<div id="footerTop">
+        <div id="serverBox">
+            <div>
+           <h2 class="h2">Client Service</h2>
+                 <ul class="list">
+                        <li>My Account</li>
+                        <li>Track My Order</li>
+                        <li>Returns &amp; Exchanges</li>
+                        <li>shipShipping &amp; Payments</li>
+                        <li>Size &amp; Alterations</li>
+                        <li>FAQs</li>
+                        <li>About Us</li>
+                        </ul>
+                </div>
+            </div>
+            <div id="connectBox">
+                <div class="box">
+                    <h2 class="h2">Connect us</h2>
+                    <ul class="list">
+                        <li>
+                            For international orders, please click here</li>
+                    </ul>
+                </div>
+                <div class="box">
+                    <h2 class="h2">Shop</h2>
+                    <ul class="list">
+                        <li>New Arrivals</li>
+                        <li>Women</li>
+                        <li>Men</li>
+                        <li>Bags</li>
+                    </ul>
+                </div>
+            </div>
+         <div id="singUpBox">
+        <div id="upperBox">
+        <h2 class="h2">Email Sign-Up</h2>
+        <div id="fOrder"><p>Sign upto receive our latest updates and enjoy 20% off your first order.</p></div>
+        <div id="myForm">
+            <div id="forEmail">
+           
+                <input id="input1" type="text" placeholder="Enter Email Address">
+            </div>
+            <div id="forCheckBox">
+                <div id="checkboxBox">
+                    <input  id="checkbox" type="checkbox">
+                </div>
+                <div id="containt">
+                    By entering your information above, you agree to receive emails about our latest collections exclusive offers, and more. You can withdraw your consent at any time. Read our Privacy Policy. for more details. Offer Valid in the U.S. only. Not valid for existing email subscribers. Exclusions apply.
+                </div>
+                
+            </div>
+            <button id="button"><h5 id="subscribe">SUBSCRIBE</h5></button>
+        </div>
+             <div id="bottomBox">
+              <div class="iconBox1">
+                  <a class="icon" href="https://www.facebook.com/theory">
+                  	<img src="../imges/fb.png" width="21px" height="21px" alt="">
+                  </a>
+                  <a class="icon" href="https://twitter.com/theory__">
+                  	<img src="../imges/twitter.png" width="21px" height="21px" alt="">
+                  </a>
+                  <a class="icon" href="javascript:void(0);"><img src="../imges/insta.png" width="21px" height="21px"     alt=""></a>
+                  <a class="icon" href="javascript:void(0);"><img src="../imges/pintrest.png"  width="21px" height="21px"alt=""></a>
+                  <a class="icon" href="javascript:void(0);"><img src="../imges/yt.png"  width="21px" height="21px"alt=""></a>
+              </div>
+              <div class="iconBox2">
+                  <a class="googleIcon" href="https://apps.apple.com/us/app/theory/id1573300770?ign-itsct=apps_box_badge&ign-itscg=30200"><img src="../imges/apple-store-logo.png"  width="110%" height="100%"   alt=""></a>
+                  <a class="googleIcon" href="https://play.google.com/store/apps/details?id=com.fastretailing.theory&hl=en_US&gl=US"> <img src="../imges/google-store-logo.png"  width="110%" height="100%"  alt=""></a>
+              </div>
+             </div>
+     	</div>
+            
+         </div>
+        </div>
+        <hr>
+        <div id="footerBottom">
+            <div id="fb">
+                    <div class="fb1">© 2021 Theory</div>
+                    <div class="fb1">Terms of Use
+                    </div>
+                    <div class="fb1">Privacy Policy</div>
+                    <div class="fb1">Exclusions
+                    </div>
+                    <div class="fb1">Site Map</div>
+            </div>
+        </div>
+         
+     </div>`
+}
+
+
+export default footer;
