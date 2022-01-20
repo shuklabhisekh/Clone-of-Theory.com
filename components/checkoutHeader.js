e560fdd34c8efa73bbbaca35c94c4f9fb379b4c1
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-01-20 12:35:49 +0530

    header Footer Seperate by components

---
+++ components/checkoutHeader.js
@@ -0,0 +1,20 @@
+function checkoutHeader(){
+    return ` <div id="vtopblackbar">
+    <p>Free Shipping To India On All Orders</p>
+    
+</div>
+<div id="vtheoryhome">
+    <p>Theory</p>
+</div>`
+}
+
+
+function checkOutFooter(){
+   return` <p>Contact Us | Help | Terms & Conditions | Privacy Policy</p>
+   <img src="https://www.global-e.com/sectigo_trust_seal_sm_82x32.png" alt="">
+   <p>Sectigo Wildcard SSL</p>`
+}
+
+
+export {checkoutHeader, checkOutFooter}
+
