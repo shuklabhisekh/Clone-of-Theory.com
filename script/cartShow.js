3dee338c803d0d0d0aa351cbaff00ffd87a65a35
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-01-21 12:51:27 +0530

    js work

---
+++ script/cartShow.js
@@ -0,0 +1,84 @@
+// let cart = JSON.parse(localStorage.getItem("cart"));
+// console.log(cart);
+// append(cart)
+
+
+let cart = [{
+    title : "Treeca Pant in Good Wool",
+    price : "₹ 16,700.00",
+    image1 : "https://ak-media.theory.com/i/theory/TH_H0101234_A08_L0/TREECA-2?$TH-Bestseller53x$",
+    image2 : "https://ak-media.theory.com/i/theory/TH_H0101234_A08_0/TREECA-2?$TH-Bestseller53x$"
+  },
+  {
+    title : "Treeca Pant in Good Wool",
+    price : "₹ 16,700.00",
+    image1 : "https://ak-media.theory.com/i/theory/TH_H0101234_A08_L0/TREECA-2?$TH-Bestseller53x$",
+    image2 : "https://ak-media.theory.com/i/theory/TH_H0101234_A08_0/TREECA-2?$TH-Bestseller53x$"
+  },
+  {
+    title : "Treeca Pant in Good Wool",
+    price : "₹ 16,700.00",
+    image1 : "https://ak-media.theory.com/i/theory/TH_H0101234_A08_L0/TREECA-2?$TH-Bestseller53x$",
+    image2 : "https://ak-media.theory.com/i/theory/TH_H0101234_A08_0/TREECA-2?$TH-Bestseller53x$"
+  }
+]
+
+  append_final(cart)
+
+
+
+
+function append_final(el){
+    el.map((data)=>{
+
+  
+
+
+    let img = document.createElement("img");
+    img.setAttribute("class","image00v")
+    img.src = data.image2;
+
+
+    let desc = document.createElement("p")
+    desc.innerHTML=data.title;
+    let price = document.createElement("p")
+    price.innerHTML = data.price;
+    let quantity = document.createElement("div")
+    quantity.innerHTML= '1'
+    
+
+    let sTotal = document.createElement("p")
+    sTotal.setAttribute("class","sTotal")
+    sTotal.innerHTML = data.price;
+
+
+   var row = document.createElement("tr")
+   var first_col = document.createElement('td')
+   
+   var col3 = document.createElement("td")
+   col3.append(price) 
+   var col4 = document.createElement("td") 
+    col4.append(quantity)
+   var col5 = document.createElement("td") 
+   
+
+
+   var showimage = document.createElement('tr')
+   showimage.setAttribute('class', 'showimagev')
+
+
+   var col1 = document.createElement("td")
+   col1.append(img);
+   var col2 = document.createElement("td") 
+   col2.append(desc);
+
+   showimage.append(col1,col2)
+   first_col.append(showimage)
+
+
+   row.append(showimage, col4, col3,col5);
+    document.querySelector("tbody").append(row);
+
+})
+
+}
