3396e57478b9ec1aa5283a865e406959184d8f5f
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-01-22 13:27:15 +0530

    Accessories data

---
+++ scripts/accesoriesdata.js
@@ -0,0 +1,74 @@
+accessoriesdata=[
+    {
+        title:"Laced Boot in Satin Leather",
+        price:"₹ 42,200.00",
+        image1:"https://ak-media.theory.com/i/theory/TH_J07AC001_D1S_0?$TH-pdp-large-desktop$",
+        image2:"https://ak-media.theory.com/i/theory/TH_J07AC001_D1S_L0?$TH-pdp-large-desktop$",
+    },
+    {
+        title:"High Top Sneaker in Suede",
+        price:"₹ 29,400.00",
+        image1:"https://ak-media.theory.com/i/theory/TH_L10AC013_UYV_0?$TH-pdp-large-desktop$",
+        image2:"https://ak-media.theory.com/i/theory/TH_L10AC013_UYV_L0?$TH-pdp-large-desktop$"
+    },
+    {
+        title:"Slide in Shearling",
+        price:"₹ 25,200.00",
+        image1:"https://ak-media.theory.com/i/theory/TH_L10AC006_001_0?$TH-pdp-large-desktop$",
+        image2:"https://ak-media.theory.com/i/theory/TH_L10AC006_001_L0?$TH-pdp-large-desktop$"
+    },
+    {
+        title:"Simple Tote in Leather",
+        price:"₹ 33,700.00",
+        image1:"https://ak-media.theory.com/i/theory/TH_K05AC026_E05_0?$TH-pdp-large-desktop$",
+        image2:"https://ak-media.theory.com/i/theory/TH_K05AC026_E05_L0?$TH-pdp-large-desktop$"
+    },
+    {
+        title:"Small Bucket Bag in Suede",
+        price:"₹ 32,000.00",
+        image1:"https://ak-media.theory.com/i/theory/TH_L07AC001_001_0?$TH-pdp-large-desktop$",
+        image2:"https://ak-media.theory.com/i/theory/TH_L07AC001_001_L0?$TH-pdp-large-desktop$"
+    },
+    {
+        title:"Micro Bucket Bag in Shearling",
+        price:"₹ 29,400.00",
+        image1:"https://ak-media.theory.com/i/theory/TH_L10AC001_QE0_0?$TH-pdp-large-desktop$",
+        image2:"https://ak-media.theory.com/i/theory/TH_L10AC001_QE0_L0?$TH-pdp-large-desktop$"
+    },
+    {
+        title:"Classic Scarf in Cashmere",
+        price:"₹ 16,600.00",
+        image1:"https://ak-media.theory.com/i/theory/TH_K0708001_E02_0?$TH-pdp-large-desktop$",
+        image2:"https://ak-media.theory.com/i/theory/TH_K0708001_E02_L0?$TH-pdp-large-desktop$"
+    },
+    {
+        title:"Honeycomb Hat in Felted Wool-Cashmere",
+        price:"₹ 8,100.00",
+        image1:"https://ak-media.theory.com/i/theory/TH_L0911707_QBC_0?$TH-pdp-large-desktop$",
+        image2:"https://ak-media.theory.com/i/theory/TH_L0911707_QBC_L0?$TH-pdp-large-desktop$"
+    },
+    {
+        title:"Triangle Scarf in Cashmere",
+        price:"₹ 11,500.00",
+        image1:"https://ak-media.theory.com/i/theory/TH_L0918727_C05_0?$TH-pdp-large-desktop$",
+        image2:"https://ak-media.theory.com/i/theory/TH_L0918727_C05_AL0?$TH-pdp-large-desktop$"
+    },
+    {
+        title:"Folded Slide in Satin",
+        price:"₹ 10,700.00",
+        image1:"https://ak-media.theory.com/i/theory/TH_L05AC013_F0X_0?$TH-pdp-large-desktop$",
+        image2:"https://ak-media.theory.com/i/theory/TH_L05AC013_F0X_L0?$TH-pdp-large-desktop$"
+    },
+    {
+        title:"Asymmetrical Pump in Glossed Leather",
+        price:"₹ 30,300.00",
+        image1:"https://ak-media.theory.com/i/theory/TH_L07AC015_001_S0?$TH-pdp-large-desktop$",
+        image2:"https://ak-media.theory.com/i/theory/TH_L07AC015_001_L0?$TH-pdp-large-desktop$"
+    },
+    {
+        title:"Pleated Ballet Flat in Leather",
+        price:"₹ 29,400.00",
+        image1:"https://ak-media.theory.com/i/theory/TH_J04AC005_001_0?$TH-pdp-large-desktop$",
+        image2:"https://ak-media.theory.com/i/theory/TH_J04AC005_001_L0?$TH-pdp-large-desktop$"
+    }
+]
